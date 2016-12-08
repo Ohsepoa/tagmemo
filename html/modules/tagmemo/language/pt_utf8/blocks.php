c60f4787961dd358fb0d7bf062f3bb216c16e1fe
Leco1 <ohsepoa@gmail.com>
2016-12-08 14:56:08 -0200

    creat language pt_utf8
    creat language pt_utf8

---
+++ html/modules/tagmemo/language/pt_utf8/blocks.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on 2006/03/27
+ */
+// Blocks
+define('_MB_TAGMEMO_SEARCH', "Buscar");
+define('_MB_TAGMEMO_SEARCH_ALL', "Todas");
+define('_MB_TAGMEMO_SEARCH_SUB', "Nesta Lista");
+?>
