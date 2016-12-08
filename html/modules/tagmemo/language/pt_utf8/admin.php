e650d64cfc34b5b57f9c2bf297eacc8837e15b74
Leco1 <ohsepoa@gmail.com>
2016-12-08 14:53:28 -0200

    creat language pt_utf8
    creat language pt_utf8

---
+++ html/modules/tagmemo/language/pt_utf8/admin.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Created on 2006/03/31
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+define('_AM_SET_SUGGEST','Clique para ver as mesmas tags sugeridas no campo de sugestões de tags do formulario de registro.');
+define('_AM_GROUPPERM','Permissões');
+define('_AM_GROUPPERMDESC','Permitir enviar um registro');
+define('_AM_TAGMEMO_GPERM_SUBMIT','Enviar');
+?>
