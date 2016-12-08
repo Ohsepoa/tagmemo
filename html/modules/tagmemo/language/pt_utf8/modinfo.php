5e3971dc98c17c403bb06b73e87d5203cedc35eb
Leco1 <ohsepoa@gmail.com>
2016-12-08 14:59:10 -0200

    creat language pt_utf8

---
+++ html/modules/tagmemo/language/pt_utf8/modinfo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+* @package languege
+* @ignore
+*/
+define('_MI_TAGMEMO_NAME', "Tags");
+define('_MI_TAGMEMO_DESC', "Fácil de registrar tag");
+//define('_MI_TAGMEMO_SEARCH', "Buscar");
+define('_MI_TAGMEMO_POPULAR_TAG_BLOCK',"Tags Populares");
+define('_MI_TAGMEMO_RECENT_TAG_BLOCK',"Últimas Tags");
+define('_MI_TAGMEMO_SERCH_TAG_BLOCK',"Buscar tags registradas");
+define('_MI_TAGMEMO_TAGCLOUD_BLOCK',"Nuvem de tags");
+//define('_MI_TAGMEMO_SEARCH_ALL', "Todas");
+//define('_MI_TAGMEMO_SEARCH_SUB', "Nesta lista");
+define('_MI_TAGMEMO_USE_FF_PLUGIN', "Usar plugin de busca do firefox");
+define('_MI_TAGMEMO_PER_PAGE', "Registros por página");
+define('_MI_TAGMEMO_USE_AUTOLINK', "Tags com Autolink relacionadas aos registros");
+define('_MI_TAGMEMO_KAKASI_PATH', "'Percurso do KAKASI");
+define('_MI_TAGMEMO_TAGPOPUP_LIST', "Popup quando clicar no link da tag (Lista)");
+define('_MI_TAGMEMO_TAGPOPUP_DETAIL', " Popup quando clicar no link da tag (Detalhe)");
+define('_MI_TAGMEMO_TAGPOPUP_CLOUD', " Popup quando clicar no link da tag (Nuvem)");
+define('_MI_TAGMEMO_SHORTURL', "Habilitar URL curta");
+define('_MI_TAGMEMO_SEARCH_TAG', "Nome da tag está incluida na busca XOOPS.");
+
+define('_MI_TAGMEMO_ADMENU1','Index');
+define('_MI_TAGMEMO_ADMENU2','Blocos e Grupos');
+define('_MI_TAGMEMO_ADMENU3','Permissões');
+define('_MI_TAGMEMO_UPDATE','Atualização');
+define('_MI_TAGMEMO_GOMODULES','Ir ao módulo');
+
+define('_MI_TAGMEMO_LIST', "Lista de Registros");
+define('_MI_TAGMEMO_EDIT', "Postar novo registro");
+define('_MI_TAGMEMO_CLOUD', "Nuvem de tags");
+
+define('_MI_TAGMEMO_ADMENU_MYLANGADMIN',  'Linguagens');
+define('_MI_TAGMEMO_ADMENU_MYTPLSADMIN',  'Modelos');
+define('_MI_TAGMEMO_ADMENU_MYBLOCKSADMIN','Blocos/Permissões');
+define('_MI_TAGMEMO_ADMENU_MYPREFERENCES','Preferências');
+?>
