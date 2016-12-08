45404bd4dad200843ab569d214e14ce543503913
Leco1 <ohsepoa@gmail.com>
2016-12-08 14:57:47 -0200

    creat language pt_utf8
    creat language pt_utf8

---
+++ html/modules/tagmemo/language/pt_utf8/main.php
@@ -0,0 +1,42 @@
+<?php
+/**
+* @package languege
+* @ignore
+*/
+
+define('_MD_TAGMEMO_CREATE', "Tags");
+define('_MD_TAGMEMO_TITLE', "Titulo");
+define('_MD_TAGMEMO_TAGS', "Tag");
+define('_MD_TAGMEMO_DESC', "Descrição");
+define('_MD_TAGMEMO_CANCELL', "Cancelar");
+define('_MD_TAGMEMO_DELETE', "Deletar");
+define('_MD_TAGMEMO_SAVE', "Autosave");
+define('_MD_TAGMEMO_DONE', "Savar/Fechar");
+define('_MD_TAGMEMO_PUBLIC', "Configurar para público");
+define('_MD_TAGMEMO_ON', "Público");
+//define('_MD_TAGMEMO_OFF', "Privado");
+define('_MD_TAGMEMO_DATETIME', "Datetime"); // check
+define('_MD_TAGMEMO_EDIT', "Editar");
+define('_MD_TAGMEMO_MESSAGE_SAVE', "Salvo"); // check
+define('_MD_TAGMEMO_MESSAGE_DELETE', "Deletado"); // check
+define('_MD_TAGMEMO_LIST_E', "Lista");
+define('_MD_TAGMEMO_EDIT_E', "Editar");
+define('_MD_TAGMEMO_EDIT_W', "Escrever");
+define('_MD_TAGMEMO_TAGS_E', "Tags");
+define('_MD_TAGMEMO_TAGS_N', "Novo");
+define('_MD_TAGMEMO_TAGS_P', "Popular");
+define('_MD_TAGMEMO_MESSAGE_DELETE_CONFIRM', "Você tem certeza que deseja deletar este registro?"); // check
+define('_MD_TAGMEMO_IMG_LIST', "Ir para a lista de registros");
+define('_MD_TAGMEMO_IMG_EDIT', "Escrever novo registro ");
+define('_MD_TAGMEMO_IMG_CLOUD', "Ir para a nuvem de tags");
+define('_MD_TAGMEMO_TAGCLICK_DELETE', "Clique para deletar");
+define('_MD_TAGMEMO_PLUGIN_FFSEARCH', "Adicionar plugin de busca do Firefox");
+define('_MD_TAGMEMO_BOOKMARKLET', "Favorito");
+//define('_MD_TAGMEMO_SEARCH', "Buscar");
+//define('_MD_TAGMEMO_SEARCH_ALL', "Todos");
+//define('_MD_TAGMEMO_SEARCH_SUB', "Nesta lista");
+define('_MD_TAGMEMO_CLOSE2OPEN', "Um favorito já está aberto. Fechar ele para um novo?");
+define('_MD_TAGMEMO_NOTWORKFRAME', "Favorito não pode trabalhar em uma página de frame.");
+define('_MD_TAGMEMO_RETRYPLEASE', "Não é possível mostrar um favorito. Por favor, registre novamente.");
+define('_MD_TAGMEMO_ENCODEHINT', "ENCODEHINT ");
+?>
